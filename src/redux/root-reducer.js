c31975fdc1dab45bedca536b5b2221fc6f40c04f
samanthasang <samanthasang70@gmail.com>
2023-08-15 07:57:04 +0330

    Remove: persist

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,18 +1,10 @@
 import { combineReducers } from 'redux';
-// import { persistReducer } from 'redux-persist';
-// import storage from 'redux-persist/lib/storage';
 
 import cartReducer from './cart/cart.reducer';
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   whitelist: ['cart']
-// };
 
 const rootReducer = combineReducers({
   cart: cartReducer,
 });
 
-// export default persistReducer(persistConfig, rootReducer);
 export default rootReducer;
